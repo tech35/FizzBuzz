301d891e101ada39afee584f7daf58a209fd4835
Bilawal Asghar <asgharbilawal6@gmail.com>
2021-02-09 14:58:51 -0800

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+for fizzbuzz in range(101):
+	if fizzbuzz %3 == 0 and fizzbuzz %5 == 0:
+		print("FizzBuzz")
+		continue
+	elif fizzbuzz %3 == 0:
+		print("Fizz")
+		continue
+	elif fizzbuzz %5 == 0:
+		print("Buzz")
+		continue
+	print("FizzBuzz")
